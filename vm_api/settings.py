f7344622d8e86c1cb01c39ce595be751fde22b53
zhanguoxing <710800324@qq.com>
2019-11-06 09:47:31 +0800

    v1.0

--- vm_api/settings.py
+++ vm_api/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 QEMU_URL = 'qemu+tcp://192.168.55.58:16509/system'
 IMG_PATH = '/ddhome/kvm/images'
--- vm_api/urls.py
+++ vm_api/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api import views
+from django.conf import settings
+from django.views import static
 
 urlpatterns = [
     #path('admin/', admin.site.urls)
     path('', views.Root.as_view()),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    re_path(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT})
 ]
